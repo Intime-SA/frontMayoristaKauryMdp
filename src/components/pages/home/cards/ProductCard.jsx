0527e4baa675d05959bb3a501407670bd2a8e712
Intime-SA <invertimesa@gmail.com>
2024-04-16 01:03:22 -0300

    v2

--- src/components/pages/home/cards/ProductCard.jsx
+++ src/components/pages/home/cards/ProductCard.jsx
@@ -34,7 +34,7 @@ const ProductCard = ({ product, handleClick, oferta }) => {
           {imageLoaded ? (
             <CardMedia
               component="img"
-              height={product.imageCard ? "300" : "300px"}
+              height="auto"
               image={
                 product.imageCard ||
                 "https://firebasestorage.googleapis.com/v0/b/mayoristakaurymdp.appspot.com/o/Mayorista%20Mar%20del%20Plata%20(2).png?alt=media&token=87bdf689-8eb7-49b1-9317-f6a52a9a0781"
